a48a77b0773e418590cd68a656c08a3962fcb824
bayashi <bayashi@cpan.org>
2013-09-08 18:15:53 +0900

    first off

---
+++ example/app.psgi
@@ -0,0 +1,49 @@
+use strict;
+use warnings;
+use Plack::Builder;
+
+builder {
+    enable 'DevFilter',
+        filters => [
+            {
+                match => sub {
+                    my ($self, $env, $res) = @_;
+                    return 1 if $env->{PATH_INFO} eq '/favicon.ico';
+                },
+                proc  => sub {
+                    my ($self, $env, $res,
+                            $body_ref, $imager, $image_type) = @_;
+                    if ($imager) {
+                        $imager = $imager->convert(preset => 'gray')
+                                        or die Imager->errstr;
+                        my $out;
+                        $imager->write(data => \$out, type => $image_type);
+                        $res->[2] = [$out];
+                    }
+                },
+            },
+            {
+                match => sub {
+                    my ($self, $env, $res) = @_;
+                    return 1 if $env->{PATH_INFO} eq '/';
+                },
+                proc  => sub {
+                    my ($self, $env, $res,
+                            $body_ref, $imager, $image_type) = @_;
+                    $$body_ref .= ' How are you?';
+                    $res->[2] = [$$body_ref];
+                },
+            },
+        ],
+    ;
+
+    enable 'Static',
+        path => qr{/favicon\.ico$},
+        root => 'share',
+    ;
+
+    return sub {
+        my $plack_env = $ENV{PLACK_ENV} || '';
+        [200, ['Content-Type' => 'text/plain'], ["Hello, $plack_env!"]];
+    };
+};
--- lib/Plack/Middleware/DevFilter.pm
+++ lib/Plack/Middleware/DevFilter.pm
@@ -2,14 +2,65 @@ package Plack::Middleware::DevFilter;
 use strict;
 use warnings;
 use Carp qw/croak/;
+use parent 'Plack::Middleware';
+use Plack::Util::Accessor qw/force_enable filters image_type/;
+use Plack::Util;
 
 our $VERSION = '0.01';
 
-sub new {
-    my $class = shift;
-    my $args  = shift || +{};
+sub prepare_app {
+    my $self = shift;
 
-    bless $args, $class;
+    croak "'filters' option must be ARRAY." unless ref $self->filters eq 'ARRAY';
+
+    unless ($self->image_type) {
+        $self->image_type(
+            sub {
+                my ($env, $res) = @_;
+
+                my $ct = Plack::Util::header_get($res->[1], 'content-type');
+                my $image_type = ($ct =~ m!(png|gif|jpeg|ico)!) ? $1 : undef;
+                return $image_type;
+            },
+        );
+    }
+}
+
+sub call {
+    my($self, $env) = @_;
+
+    my $res = $self->app->($env);
+
+    if ( !$self->force_enable
+            && (!$ENV{PLACK_ENV} || $ENV{PLACK_ENV} !~ m!^(?:development|test)$!) ) {
+        return $res;
+    }
+
+    my $body = '';
+    Plack::Util::foreach($res->[2], sub {
+        my($buf) = @_;
+        $body .= $buf;
+    });
+
+    my ($imager, $image_type);
+    if ( $image_type = $self->image_type->($env, $res) ) {
+        require Imager;
+        $imager = Imager->new(
+            data => $body,
+            type => $image_type,
+        ) or croak Imager->errstr;
+    }
+
+    my $filtered = 0;
+    for my $filter (@{$self->filters}) {
+        if ( $filter->{match}->($self, $env) ) {
+            $filter->{proc}->($self, $env, $res,
+                                    \$body, $imager, $image_type);
+            $filtered = 1;
+        }
+    }
+
+    return $filtered ? $res : [ $res->[0], $res->[1], [$body] ];
 }
 
 1;
@@ -23,8 +74,31 @@ Plack::Middleware::DevFilter - one line description
 
 =head1 SYNOPSIS
 
-    use Plack::Middleware::DevFilter;
+    use Plack::Builder;
 
+    builder {
+        enable 'DevFilter',
+            filters => [
+                { # favicon.ico
+                    match => sub {
+                        my ($self, $env, $res) = @_;
+                        return 1 if $env->{PATH_INFO} eq '/favicon.ico';
+                    },
+                    proc  => sub {
+                        my ($self, $env, $res,
+                                $body_ref, $imager, $image_type) = @_;
+                        if ($imager) {
+                            $imager = $imager->convert(preset => 'gray')
+                                            or die Imager->errstr;
+                            my $out;
+                            $imager->write(data => \$out, type => $image_type);
+                            $res->[2] = [$out];
+                        }
+                    },
+                },
+            ],
+        ;
+    };
 
 =head1 DESCRIPTION
 
@@ -46,7 +120,11 @@ Dai Okabayashi E<lt>bayashi@cpan.orgE<gt>
 
 =head1 SEE ALSO
 
-L<Other::Module>
+L<Plack::Middleware>
+
+L<Imager>
+
+This module was inspired by L<Plack::Middleware::DevFavicon>.
 
 
 =head1 LICENSE
